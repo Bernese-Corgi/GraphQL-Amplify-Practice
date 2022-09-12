608c6a8cb61c99609a09553e20a693d1773e2d17
Bernese-Corgi <jnyng0621@gmail.com>
2022-09-12 15:43:55 +0900

    resolver 작성 - Query 작성

--- tweetql/server.js
+++ tweetql/server.js
@@ -1,5 +1,12 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const tweets = [
+  { id: '1', text: 'hello 1' },
+  { id: '2', text: 'hello 2' },
+  { id: '3', text: 'hello 3' },
+  { id: '4', text: 'hello 4' },
+]
+
 const typeDefs = gql`
   type User {
     id: ID!
@@ -9,13 +16,13 @@ const typeDefs = gql`
   type Tweet {
     id: ID!
     text: String!
-    author: User!
+    author: User
   }
 
   # 루트 타입 : 타입들의 집합
   type Query {
     allTweets: [Tweet!] # /tweets
-    tweet(id: ID!): Tweet! # /tweet/:id 라고 생각할 수 있다.
+    tweet(id: ID!): Tweet # /tweet/:id 라고 생각할 수 있다.
   }
 
   # 뮤테이션
@@ -25,7 +32,19 @@ const typeDefs = gql`
   }
 `
 
-const server = new ApolloServer({ typeDefs })
+const resolvers = {
+  // 타입 정의와 같은 형태를 가져야한다.
+  Query: {
+    allTweets() {
+      return tweets
+    },
+    tweet(root, { id }) {
+      return tweets.find(tweet => tweet.id === id)
+    },
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
