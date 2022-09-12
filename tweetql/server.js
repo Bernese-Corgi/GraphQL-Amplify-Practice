e694b1b685b59fac8defbca9b8ae95cc12fcb395
Marcella <marcella@jejememe.com>
2022-09-12 15:27:02 +0900

    [tweetql] non-nullable 타입 설정

--- tweetql/server.js
+++ tweetql/server.js
@@ -2,26 +2,26 @@ import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
   type User {
-    id: ID
-    username: String
+    id: ID!
+    username: String!
   }
 
   type Tweet {
-    id: ID
-    text: String
-    author: User
+    id: ID!
+    text: String!
+    author: User!
   }
 
   # 루트 타입 : 타입들의 집합
   type Query {
-    allTweets: [Tweet] # /tweets
-    tweet(id: ID): Tweet # /tweet/:id 라고 생각할 수 있다.
+    allTweets: [Tweet!] # /tweets
+    tweet(id: ID!): Tweet! # /tweet/:id 라고 생각할 수 있다.
   }
 
   # 뮤테이션
   type Mutation {
-    postTweet(text: String, userId: ID): Tweet
-    deleteTweet(id: ID): Boolean
+    postTweet(text: String!, userId: ID!): Tweet
+    deleteTweet(id: ID!): Boolean
   }
 `
 
